7d6bf490b0474758b3149e3a860e15725dd96ec4
Dave Houston <root@crankyadmin.net>
2016-08-06 20:16:15 +0100

    Turn ligitures on.
    Currently doesn't check if you're on a mac or not, if you're not you'll
    probably want to turn this off.

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/cask")
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
